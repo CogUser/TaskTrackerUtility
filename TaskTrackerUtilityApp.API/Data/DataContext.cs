bf1389bc8d1bd5fee02eb94fbb4f0217c4c6f5d1
Sup682 <supriya88swami@gmail.com>
2020-04-21 20:32:06 +0530

    Initial Commit

---
+++ TaskTrackerUtilityApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TaskTrackerUtilityApp.API.Models;
+
+namespace TaskTrackerUtilityApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+       public DataContext(DbContextOptions<DataContext> options) : base(options){} 
+
+       public DbSet<Value> Values {get; set;}
+    }
+}
